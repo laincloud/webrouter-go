40aef54fa2b44b750ca960ab7acca48437a32d9f
liuxu <liuxu623@gmail.com>
2018-03-21 12:12:05 +0800

    move tmpl files to nginx path

--- nginx/config.go
+++ nginx/config.go
@@ -4,7 +4,6 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"github.com/facebookgo/pidfile"
-	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -14,109 +13,6 @@ import (
 
 var nginxConfTmpl, upstreamTmpl, serverTmpl *template.Template
 var certs map[string]*x509.Certificate
-
-func init() {
-	var err error
-	upstreamTmpl, err = template.New("upstream").Parse(`
-{{ range $name, $uri := $.Upstreams -}}
-upstream {{ $name }} {
-    # fake server otherwise ngx_http_upstream will report error when startup
-    server 127.0.0.1:11111;
-    
-    # all backend server will pull from consul when startup and will delete fake server
-	upsync {{ $.ConsulAddr }}/v1/kv/{{ $.ConsulPrefix }}{{ $name }}/ upsync_timeout=6m upsync_interval=500ms upsync_type=consul strong_dependency=off;
-	upsync_dump_path /usr/local/openresty/nginx/upstreams/{{ $name }}.upstream;
-{{ if $uri }}
-	check interval=3000 rise=2 fall=5 timeout=1000 type=http;
-	check_http_send "GET {{ $uri }} HTTP/1.0\r\n\r\n";
-	check_http_expect_alive http_2xx http_3xx;
-{{ end -}}
-}
-
-{{ end -}}`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	serverTmpl, err = template.New("server").Parse(`
-{{ range $serverName, $server := $.Servers -}}
-server {
-    listen  80;
-    {{ if $server.SSL -}}
-    listen 443;
-    {{ end -}}
-    server_name  .{{ $serverName }};
-    {{ if $server.SSL -}}
-    ssl_certificate {{ $.NginxPath }}/ssl/{{ $server.SSL }}.crt;
-    ssl_certificate_key {{ $.NginxPath }}/ssl/{{ $server.SSL }}.key;
-    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-    ssl_ciphers  HIGH:!aNULL:!MD5;
-    {{ end -}}
-    include proxy.conf;
-{{ range $uri, $location := $server.Locations -}}
-{{ if eq $uri "/" }}
-    location / {
-        proxy_pass  http://{{ $location.Upstream }};
-        access_log  {{ $.LogPath }}{{ $serverName }}___{{ $location.Upstream }}.access.log  main;
-    }
-{{ else }}
-    location /{{ $uri }}/ {
-        rewrite /{{ $uri }}/(.*) /$1 break;
-        proxy_pass  http://{{ $location.Upstream }};
-        access_log  {{ $.LogPath }}{{ $serverName }}___{{ $location.Upstream }}.access.log  main;
-    }
-{{ end -}}
-{{ end -}}
-}
-
-{{ end -}}`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	nginxConfTmpl, err = template.New("nginx").Parse(`
-worker_processes  4;
-
-error_log  {{ $.LogPath }}error.log warn;
-pid        {{ $.PidPath }};
-
-events {
-    worker_connections  1024;
-}
-
-http {
-    include       mime.types;
-    default_type  application/octet-stream;
-
-    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-                      '$status $body_bytes_sent "$http_referer" '
-                      '"$http_user_agent" "$http_x_forwarded_for"';
-
-    access_log  {{ $.LogPath }}access.log  main;
-
-    sendfile        on;
-
-    keepalive_timeout  65;
-
-    server_names_hash_max_size 1024;
-
-    server {
-        listen 80;
-        server_name  .{{ $.ServerName }};
-        location / {
-            upstream_show;
-        }
-    }
-
-    include {{ $.NginxPath }}conf/server.conf;
-    include {{ $.NginxPath }}conf/upstream.conf;
-}`)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	certs = make(map[string]*x509.Certificate)
-}
 
 type Location struct {
 	Upstream  string
@@ -134,19 +30,40 @@ type Config struct {
 }
 
 func Init(nginxPath string, logPath string, serverName string, pidPath string) error {
+	var err error
+
+	nginxConfTmpl, err = template.ParseFiles(nginxPath + "tmpl/nginx.conf.tmpl")
+	if err != nil {
+		return err
+	}
+
+	upstreamTmpl, err = template.ParseFiles(nginxPath + "tmpl/upstream.conf.tmpl")
+	if err != nil {
+		return err
+	}
+
+	serverTmpl, err = template.ParseFiles(nginxPath + "tmpl/server.conf.tmpl")
+	if err != nil {
+		return err
+	}
+
 	if err := renderNginxConf(nginxPath, logPath, pidPath, serverName); err != nil {
 		return err
 	}
+
 	if err := loadCrt(nginxPath); err != nil {
 		return err
 	}
+
 	if _, err := os.Create(nginxPath + "conf/server.conf"); err != nil {
 		return err
 	}
+
 	if _, err := os.Create(nginxPath + "conf/upstream.conf"); err != nil {
 		return err
 	}
-	_, err := os.Stat(nginxPath + "upstreams")
+
+	_, err = os.Stat(nginxPath + "upstreams")
 	if os.IsNotExist(err) {
 		if err := os.Mkdir(nginxPath+"upstreams", os.ModePerm); err != nil {
 			return err
@@ -154,9 +71,11 @@ func Init(nginxPath string, logPath string, serverName string, pidPath string) e
 	} else {
 		return err
 	}
+
 	if _, err := os.Create(nginxPath + "lock"); err != nil {
 		return err
 	}
+
 	return nil
 }
 
@@ -213,6 +132,7 @@ func renderUpstreamConf(config *Config, consulAddr string, consulPrefix string, 
 }
 
 func loadCrt(nginxPath string) error {
+	certs = make(map[string]*x509.Certificate)
 	files, err := ioutil.ReadDir(nginxPath + "ssl")
 	if err != nil {
 		return err
